26591d4c4b7b4219e9875b67afa2e54da19e58f4
Tóth Csaba <tocsamanatee@gmail.com>
2020-11-28 15:56:55 +0100

    Initialized Swagger

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,28 +1,15 @@
 package org.example;
 
-import org.example.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class App implements CommandLineRunner
-{
+public class App {
 
-    @Autowired
-    ApplicationContext context;
-
-    public static void main( String[] args )
+    public static void main( String[] args)
     {
         SpringApplication.run(App.class, args);
     }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Hello Spring!");
-        CustomerService customerService = context.getBean(CustomerService.class);
-        customerService.getAllCustomers().forEach(System.out::println);
-    }
 }
+
